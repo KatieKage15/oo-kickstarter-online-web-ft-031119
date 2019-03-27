054ef58476ef54c187f9187da272f1f625442e16
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-27 13:15:22 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,15 @@
+
 class Backer
+attr_reader :name, :backed_projects
 
-  attr_reader :backed_projects, :name
+def initialize(name)
+  @name = name
+  @backed_projects = []
+end
 
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
+def back_project(project)
+  backed_projects << project
+  project.backers << self
+end
 
-  def back_project(projects)
-    @backed_projects << projects
-    project.backers << self
-  end
 end
